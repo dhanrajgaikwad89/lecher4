0372e7021afa96e3721a9df12443f356a4561d2d
Dhanraj Gaikwad <dhanraj.gaikwad771@gmail.com>
2023-07-05 14:31:50 +0530

    Commitfirst

---
+++ loop.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class loop{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        //int m = sc.nextInt();
+        // for(int i=1;i<=n;i++){
+        //     if(i%2==0){
+        //         System.out.println("Apana College ");
+        //     }
+        // }
+        int count = 0;
+        for(int i=2;i<n;i++){
+            if(n%i==0){
+                            count++;
+                        }
+        }
+        if(count<=1){
+            System.out.println("Prime");
+        }
+        else{  
+            System.out.println("not prime");
+
+        }
+        
+        
+
+
+        
+}
+}
